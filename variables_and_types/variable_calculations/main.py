83f9ac03a98c78f2eaefbe00e13f94754c263185
skhann1616 <>
2026-03-10 16:02:01 +0000

    Variable calculations

---
+++ variables_and_types/variable_calculations/main.py
@@ -0,0 +1,8 @@
+# Calculate the total cost of 100 cupcakes
+item_name = "Soda"
+item_price = 6.99
+purchase_quantity = 5
+total_cost = item_price * purchase_quantity
+
+print("Total cost for", purchase_quantity, "cupcakes is $", total_cost)
+

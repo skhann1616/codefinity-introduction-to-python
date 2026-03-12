0982a940ac520e29f05d48fda92554b188211251
skhann1616 <>
2026-03-12 23:03:26 +0000

    Challenge: Solo Coding Capstone

---
+++ loops/challenge_solo_coding_capstone/main.py
@@ -0,0 +1,19 @@
+# Inventory dictionary with stock, price, and discount price
+inventory = {
+    "Bread": [42, 1.20, 0.99],  # "Item": [current stock, regular price, discounted price]
+    "Eggs": [225, 2.12, 1.99],  # Eggs should be sold at a discount
+    "Apples": [9, 1.50, 1.35]   # Apples need to be restocked
+}
+
+for item in inventory:
+    [curr_stock,reg_price,disc_price] = inventory[item]
+    if curr_stock < 30:
+        print(f"{item} need restocking.")
+    elif curr_stock > 100:
+        print(f"{item} should be sold at the discounted price of {disc_price}.")
+    elif curr_stock >30 and curr_stock < 100:
+        print(f"{item} should be sold at the regular price of {reg_price}.")
+        
+
+
+    
